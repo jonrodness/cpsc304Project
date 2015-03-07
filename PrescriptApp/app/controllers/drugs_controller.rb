d7a9592a9ce92f2de4bc5e3026ccccdf58f6d22a
jonrodness <jon.rodness@gmail.com>
2015-03-06 18:50:38 -0800

    bare bones

--- PrescriptApp/app/controllers/drugs_controller.rb
+++ PrescriptApp/app/controllers/drugs_controller.rb
@@ -2,7 +2,8 @@ class DrugsController < ApplicationController
 
 
 	def index
-		@drugs = Drug.all
+		@drugs = Drug.all 
+		#Drug.find_by_sql "SELECT d.generic_name, d.price FROM Drug d;"
 	end
 
 end
--- PrescriptApp/app/models/drug.rb
+++ PrescriptApp/app/models/drug.rb
@@ -1,10 +1,9 @@
 class Drug  < ActiveRecord::Base 
 	
-	def self.prices
-		#static
-	end
+#	def self.prices
+#		Drug.find_by_sql "SELECT d.generic_name, d.price FROM Drug d;"
+#	end
 
 	self.table_name = "Drug"
-
 
 end
