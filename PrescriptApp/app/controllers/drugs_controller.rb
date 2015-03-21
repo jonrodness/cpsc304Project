33e2656b97794af161da21b8a2c56a897a92ce25
jonrodness <jon.rodness@gmail.com>
2015-03-21 13:14:41 -0700

    general queries working

--- PrescriptApp/app/controllers/drugs_controller.rb
+++ PrescriptApp/app/controllers/drugs_controller.rb
@@ -2,7 +2,9 @@ class DrugsController < ApplicationController
 
 
 	def index
-		@drugs = Drug.all 
+		@drugs = Drug.all
+		#@drugs = Drug.find_by_sql("SELECT * FROM Drug")
+		@result = Drug.connection.select_all("SELECT * FROM Drug")
 		#Drug.find_by_sql "SELECT d.generic_name, d.price FROM Drug d;"
 	end
 
