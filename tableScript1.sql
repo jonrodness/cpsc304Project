ed9edd29d42087322fc9e83aa10d56752025610c
chr1slou1e <chris_louie@live.ca>
2015-03-21 17:38:52 -0700

    query update

--- tableScript1.sql
+++ tableScript1.sql
@@ -17,6 +17,7 @@ drop table TimeBlock;
 
 # ---------------- CREATION ---------------- #
 
+#Doctor(LicenseNum, FirstName, LastName,Address, PhoneNumber, Type)
 CREATE TABLE Doctor 
     (LicenseNum VARCHAR(20),
     FirstName VARCHAR(20),
@@ -29,6 +30,7 @@ CREATE TABLE Doctor
 
 grant select on Doctor to public;
 
+#Patient(CareCardNum,FirstName,LastName,Age,Weight,Height,Address,PhoneNumber )
  CREATE TABLE Patient
     (CareCardNum CHAR(10),
     FirstName VARCHAR(20),    
@@ -44,6 +46,7 @@ grant select on Doctor to public;
 grant select on Patient to public;               
 
 #tinyint 0 false, 1 true
+#Prescription (LicenseNum, PrescriptID,Refills,Dosage,CareCardNum,ReadyForPickUp,date_prescribed)
 CREATE TABLE Prescription 
     (LicenseNum CHAR(10),
     PrescriptID CHAR(10),  
@@ -59,6 +62,7 @@ CREATE TABLE Prescription
 
 grant select on Prescription to public;
 
+#Drug(BrandName, GenericName, CompanyName,Price)
 CREATE TABLE Drug
     (BrandName VARCHAR(30),
     GenericName VARCHAR(30),
@@ -68,6 +72,7 @@ CREATE TABLE Drug
 
 grant select on Drug to public;
 
+#Includes(PrescriptID, BrandName, GenericName)
 CREATE TABLE Includes
     (PrescriptID CHAR(4),
     BrandName VARCHAR(30),    
@@ -78,6 +83,7 @@ CREATE TABLE Includes
 
 grant select on Includes to public;
 
+#InteractsWith(dBrandName, dGenericName, iBrandName,iGenericName)
 CREATE TABLE InteractsWith
     (dBrandName VARCHAR(30),
     dGenericName VARCHAR(30),
@@ -89,6 +95,7 @@ CREATE TABLE InteractsWith
 
 grant select on InteractsWith to public;
 
+#Pharmacy(Address,Name,PhoneNumber,WeekdayHoursOpening,WeekdayHoursClosing, WeekendHoursOpening,WeekendHoursClosing)
 CREATE TABLE Pharmacy
     (Address VARCHAR(50),
     Name VARCHAR(40),  
@@ -102,6 +109,7 @@ CREATE TABLE Pharmacy
 
 grant select on Pharmacy to public;
 
+#OrderedFrom(PrescriptID,PharmacyAddress, OrderNo )
 CREATE TABLE OrderedFrom
     (PrescriptID CHAR(4),
     PharmacyAddress VARCHAR(50),
@@ -113,6 +121,7 @@ CREATE TABLE OrderedFrom
 
 grant select on OrderedFrom to public;
 
+#TimeBlock(TimeBlockDate,StartTime,EndTime )
 CREATE TABLE TimeBlock
     (TimeBlockDate DATE,
     StartTime TIME,
@@ -121,6 +130,7 @@ CREATE TABLE TimeBlock
 
 grant select on TimeBlock to public;
 
+#MakesAppointmentWith(TimeMade, DateMade,LicenseNum,TimeBlockDate, StartTime, EndTime, CareCardNum)
 CREATE TABLE MakesAppointmentWith
     (TimeMade TIME,
     DateMade DATE,
@@ -136,6 +146,7 @@ CREATE TABLE MakesAppointmentWith
 
 grant select on MakesAppointmentWith to public;
 
+#Doctor(LicenseNum, FirstName, LastName,Address, PhoneNumber, Type)
 
 INSERT INTO Doctor
 VALUES ('1232131241', 'Bob', 'Smith', '1164 Robson St, Vancouver, BC V6E 1B2',
@@ -158,6 +169,7 @@ INSERT INTO Doctor
 VALUES ('2743873823', 'Kaneonuskatew', 'Kawacatoose', '307-1220 Pender St E, Vancouver, BC V6A 1W8', 
         '7782391233', 'Gastroenterologist');
 
+#Patient(CareCardNum,FirstName,LastName,Age,Weight,Height,Address,PhoneNumber )
 INSERT INTO Patient
 VALUES ('1234567890', 'Anny', 'Gakhokidze', '19', '90', '160', 
         '23 12746 102nd street, Surrey, BC, V3T1V9', '7783334455');
@@ -180,6 +192,8 @@ VALUES ('1099282394', 'Jane', 'Doe', '50', '56', '150',
 
 
 # prescriptions by anny
+#Prescription (LicenseNum, PrescriptID,Refills,Dosage,CareCardNum,ReadyForPickUp,date_prescribed)
+
 INSERT INTO Prescription
 VALUES ('1232131241', '2345', '10', '4 pills 2 times per day for 10 days', 
        '1234567890', '1', '2012-08-26');
@@ -216,6 +230,8 @@ VALUES ('2743873823', '0098', '2', '1 pill 3 times per day for 10 days',
 INSERT INTO Prescription
 VALUES ('3409389847', '0045', '3', '1 pill 12 times per day for 3 days', 
        '1099282394', '1', '2012-12-21');
+
+#Drug(BrandName, GenericName, CompanyName,Price)
 
 INSERT INTO Drug 
 VALUES ('Tylenol', 'Acetaminophen', 'Johnson and Johnson', '10');
@@ -256,6 +272,8 @@ VALUES ('Biaxin','Clarithromycin','Abbott Laboratories','50');
 INSERT INTO Drug 
 VALUES ('Zocor', 'Simvastatin','Pfizer','99');
 
+#InteractsWith(dBrandName, dGenericName, iBrandName,iGenericName)
+
 INSERT INTO InteractsWith
 VALUES ('Advil', 'Ibuprofen', 'Coumadin', 'Warfarin');
 
@@ -270,6 +288,8 @@ VALUES ('Niaspan','Niacin','Lipitor','Atorvastatin');
 
 INSERT INTO InteractsWith
 VALUES ('Viagra', 'Sildenafil','Biaxin','Clarithromycin');
+
+#Includes(PrescriptID, BrandName, GenericName)
 
 INSERT INTO Includes
 VALUES('2345', 'Tylenol','Acetaminophen');
@@ -295,6 +315,7 @@ VALUES ('0002', 'Coumadin','Warfarin');
 INSERT INTO Includes
 VALUES ('0003', 'Coumadin','Warfarin');
 
+#Pharmacy(Address,Name,PhoneNumber,WeekdayHoursOpening,WeekdayHoursClosing, WeekendHoursOpening,WeekendHoursClosing)
 
 INSERT INTO Pharmacy
 VALUES ('885 Broadway W, Vancouver, BC V5Z 1J9','Shoppers Drug Mart', '6047081135', 
@@ -316,7 +337,9 @@ INSERT INTO Pharmacy
 VALUES ('6180 Fraser St, Vancouver, BC V5W 3A1', 'The Medicine Shoppe Pharmacy', 
         '6042333233','08:00:00','22:30:00','11:00:00','18:00:00');
 
+#OrderedFrom(PrescriptID,PharmacyAddress, OrderNo )
 
+#TimeBlock(TimeBlockDate,StartTime,EndTime )
 INSERT INTO TimeBlock
 VALUES ('2015-04-03', '09:00:00', '10:00:00');
 
@@ -331,6 +354,8 @@ VALUES ('2015-10-24', '15:00:00', '16:00:00');
 
 INSERT INTO TimeBlock
 VALUES ('2015-07-04', '16:00:00', '16:30:00');
+
+#MakesAppointmentWith(TimeMade, DateMade,LicenseNum,TimeBlockDate, StartTime, EndTime, CareCardNum)
 
 INSERT INTO MakesAppointmentWith
 VALUES ('09:00:00', '2015-04-03','1232131241','2015-04-03','09:00:00','10:00:00','1234567890');
